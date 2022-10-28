1c1663c440c9d6ebdf887063ff8f77558508c8c5
Elkhan911 <955pro@mail.ru>
2022-10-28 17:21:30 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let arr = ["zero", 1, 2, 3, 4, 5, "fr", "gr"];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+let arr1 = [1, 2, 3, 4, 5];
+let res = 0;
+
+for (let k = 0; k < arr1.length; k++) {
+  res += arr1[k];
+}
+
+console.log(res);
